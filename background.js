e70c47b4f93a9bd51ab0f0d9114e0fefb5e1fef2
lillafayette <122611360+lillafayette@users.noreply.github.com>
2024-05-17 09:25:30 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,22 @@
+const audioFiles = [
+  'sounds/sound1.mp3',
+  'sounds/sound2.mp3',
+  'sounds/sound3.mp3',
+  'sounds/sound4.mp3'
+];
 
+const audioElements = audioFiles.map(file => {
+  const audio = new Audio(chrome.runtime.getURL(file));
+  audio.loop = true;
+  return audio;
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  const { action, index } = message;
+
+  if (action === 'play') {
+    audioElements[index].play();
+  } else if (action === 'pause') {
+    audioElements[index].pause();
+  }
+});
