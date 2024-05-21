9d51e4eb6e05b571c9a803f711ad0ce6f8c76e36
lillafayette <122611360+lillafayette@users.noreply.github.com>
2024-05-22 01:43:09 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,19 @@
+let audioElements = [];
 let playing = [false, false, false, false];
+
+const audioFiles = [
+  'sounds/sound1.mp3',
+  'sounds/sound2.mp3',
+  'sounds/sound3.mp3',
+  'sounds/sound4.mp3'
+];
+
+// Tworzenie obiektów audio i ustawienie ich na zapętlenie
+audioFiles.forEach((file, index) => {
+  const audio = new Audio(chrome.runtime.getURL(file));
+  audio.loop = true;
+  audioElements[index] = audio;
+});
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   const { action, index } = message;
@@ -6,15 +21,9 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (action === 'toggle') {
     playing[index] = !playing[index];
     if (playing[index]) {
-      chrome.offscreen.createDocument({
-        url: 'offscreen.html',
-        reasons: ['AUDIO_PLAYBACK'],
-        justification: 'Play background audio'
-      }, () => {
-        chrome.runtime.sendMessage({ action: 'play', index });
-      });
+      audioElements[index].play();
     } else {
-      chrome.runtime.sendMessage({ action: 'pause', index });
+      audioElements[index].pause();
     }
     sendResponse({ playing: playing[index] });
   } else if (action === 'getState') {
