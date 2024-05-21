40e581ac409996552182f7c35617a29cf54edf80
lillafayette <122611360+lillafayette@users.noreply.github.com>
2024-05-22 01:23:49 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,46 +1,4 @@
-let audioTabs = [];
 let playing = [false, false, false, false];
-
-const audioFiles = [
-  'sounds/sound1.mp3',
-  'sounds/sound2.mp3',
-  'sounds/sound3.mp3',
-  'sounds/sound4.mp3'
-];
-
-// Tworzenie ukrytej zakładki do odtwarzania dźwięków
-function createAudioTab(index) {
-  chrome.tabs.create({ url: 'audio.html', active: false }, (tab) => {
-    audioTabs[index] = tab.id;
-    chrome.tabs.executeScript(tab.id, {
-      code: `
-        const audio = new Audio('${chrome.runtime.getURL(audioFiles[index])}');
-        audio.loop = true;
-        audio.id = 'audio-${index}';
-        document.body.appendChild(audio);
-        audio.play();
-      `
-    });
-  });
-}
-
-// Pauza ukrytej zakładki
-function pauseAudioTab(index) {
-  chrome.tabs.executeScript(audioTabs[index], {
-    code: `
-      const audio = document.getElementById('audio-${index}');
-      if (audio) {
-        audio.pause();
-      }
-    `
-  });
-}
-
-// Usunięcie ukrytej zakładki
-function removeAudioTab(index) {
-  chrome.tabs.remove(audioTabs[index]);
-  audioTabs[index] = null;
-}
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   const { action, index } = message;
@@ -48,10 +6,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (action === 'toggle') {
     playing[index] = !playing[index];
     if (playing[index]) {
-      createAudioTab(index);
+      chrome.offscreen.createDocument({
+        url: 'offscreen.html',
+        reasons: ['AUDIO_PLAYBACK'],
+        justification: 'Play background audio'
+      }, () => {
+        chrome.runtime.sendMessage({ action: 'play', index });
+      });
     } else {
-      pauseAudioTab(index);
-      removeAudioTab(index);
+      chrome.runtime.sendMessage({ action: 'pause', index });
     }
     sendResponse({ playing: playing[index] });
   } else if (action === 'getState') {
