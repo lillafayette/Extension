56a91d07f947733ea9cb2b48981fab0270c2ffd5
michal.nerwinski <michal.nerwinski@ing.com>
2024-05-23 16:11:48 +0200

    zsrdszdtdxftyfcfgy

--- background.js
+++ background.js
@@ -1,51 +1,48 @@
-let soundStates = [false, false, false, false] // Array to track if each sound is playing
-let soundVolumes = [0.5, 0.5, 0.5, 0.5] // Array to track volumes of each sound
-
+let soundStates = [false, false, false, false]
+// Array to track if each sound is playing
+let soundVolumes = [0.5, 0.5, 0.5, 0.5]
+// Array to track volumes of each sound
 const audioFiles = [
-    'sounds/sound1.mp3',
-    'sounds/sound2.mp3',
-    'sounds/sound3.mp3',
-    'sounds/sound4.mp3',
+  "sounds/sound1.mp3",
+  "sounds/sound2.mp3",
+  "sounds/sound3.mp3",
+  "sounds/sound4.mp3",
 ]
-
-// Tworzenie obiektów audio i ustawienie ich na zapętlenie
-audioFiles.forEach((file, index) => {
-    const audio = new Audio(chrome.runtime.getURL(file))
-    audio.loop = true
-    soundStates[index] = audio
+const audios = audioFiles.map((file) => {
+  const audio = new Audio(chrome.runtime.getURL(file))
+  audio.loop = true
+  return audio
 })
-
 // Initialize volumes from storage
 chrome.storage.local.get(
-    ['volume0', 'volume1', 'volume2', 'volume3'],
-    (result) => {
-        for (let i = 0; i < soundVolumes.length; i++) {
-            if (result[`volume${i}`] !== undefined) {
-                soundVolumes[i] = result[`volume${i}`]
-            }
-        }
+  ["volume0", "volume1", "volume2", "volume3"],
+  (result) => {
+    for (let i = 0; i < soundVolumes.length; i++) {
+      if (result[`volume${i}`] !== undefined) {
+        soundVolumes[i] = result[`volume${i}`]
+        audios[i].volume = soundVolumes[i]
+      }
     }
+  },
 )
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'getState') {
-        sendResponse({ playing: soundStates, volumes: soundVolumes })
-    } else if (request.action === 'toggle') {
-        const index = request.index
-        soundStates[index] = !soundStates[index]
-        // Logic to play/pause the sound
-        if (soundStates[index]) {
-            sounds[index].play()
-        } else {
-            sounds[index].pause()
-        }
-        sendResponse({ playing: soundStates[index] })
-    } else if (request.action === 'setVolume') {
-        const index = request.index
-        const volume = request.volume
-        soundVolumes[index] = volume
-        // Logic to set volume of the sound
-        sounds[index].volume = volume
-        sendResponse({ volume: soundVolumes[index] })
+  if (request.action === "getState") {
+    sendResponse({ playing: soundStates, volumes: soundVolumes })
+  } else if (request.action === "toggle") {
+    const index = request.index
+    soundStates[index] = !soundStates[index]
+    if (soundStates[index]) {
+      audios[index].play()
+    } else {
+      audios[index].pause()
     }
+    sendResponse({ playing: soundStates[index] })
+  } else if (request.action === "setVolume") {
+    const index = request.index
+    const volume = request.volume
+    soundVolumes[index] = volume
+    audios[index].volume = volume
+    chrome.storage.local.set({ [`volume${index}`]: volume })
+    sendResponse({ volume: soundVolumes[index] })
+  }
 })
