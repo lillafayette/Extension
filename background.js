1cf0d2b49e4910286f785583f23912213eed9a19
Radosław Loth <r.loth@icloud.com>
2024-05-23 21:52:11 +0200

    stashed bionic and pomodoro and music together

--- background.js
+++ background.js
@@ -1,1 +1,33 @@
-// background.js
+let audioElements = []; // tablica dla obiektów audio
+let playing = [false, false, false, false];
+
+const audioFiles = [
+  'sounds/sound1.mp3',
+  'sounds/sound2.mp3',
+  'sounds/sound3.mp3',
+  'sounds/sound4.mp3'
+];
+
+// Tworzenie obiektów audio i ustawienie ich na zapętlenie
+audioFiles.forEach((file, index) => {
+  const audio = new Audio(chrome.runtime.getURL(file));
+  audio.loop = true;
+  audioElements[index] = audio;
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  const { action, index } = message;
+ //włącz / wyłącz odtwarzanie dźwięku
+  if (action === 'toggle') {
+    playing[index] = !playing[index];
+    if (playing[index]) {
+      audioElements[index].play();
+    } else {
+      audioElements[index].pause();
+    }
+  // getState - zwraca aktualny stan odtwarzania
+    sendResponse({ playing: playing[index] });
+  } else if (action === 'getState') {
+    sendResponse({ playing });
+  }
+});
--- popup.js
+++ popup.js
@@ -1,3 +1,35 @@
+const buttons = [
+  document.getElementById('sound1'),
+  document.getElementById('sound2'),
+  document.getElementById('sound3'),
+  document.getElementById('sound4')
+];
+
+// funkcja do aktualizacji tekstu na przyciskach
+function updateButtonText(index, isPlaying) {
+  buttons[index].textContent = isPlaying ? `Pause ${index + 1}` : `Sound ${index + 1}`;
+}
+
+// wysyłanie informacji do background.js w celu uzyskania stanu odtwarzania dźwięków
+chrome.runtime.sendMessage({ action: 'getState' }, response => {
+  response.playing.forEach((isPlaying, index) => {
+    updateButtonText(index, isPlaying);
+  });
+});
+
+// nasłuchiwanie kliknięcia na przycisk, które wysyłają informację do background.js z żadaniem wyłączenia lub włączenia dźwięku i aktualizują tekst na przyciskus
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    chrome.runtime.sendMessage({ action: 'toggle', index }, response => {
+      const isPlaying = response.playing;
+      updateButtonText(index, isPlaying);
+    });
+  });
+});
+
+
+
+
 document.getElementById('toggle-bold').addEventListener('change', (event) => {
   const isChecked = event.target.checked;
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
